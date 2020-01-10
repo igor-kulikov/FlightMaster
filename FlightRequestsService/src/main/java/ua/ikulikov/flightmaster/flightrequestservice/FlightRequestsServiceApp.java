b608d6b499a53b032111912a65f9405877ecbb00
Igor Kulikov <igor.kulikov.mail@gmail.com>
2020-01-10 12:40:16 +0200

    enable Eureka clients for FlightRequestService and GeoCatalogService

--- FlightRequestsService/src/main/java/ua/ikulikov/flightmaster/flightrequestservice/FlightRequestsServiceApp.java
+++ FlightRequestsService/src/main/java/ua/ikulikov/flightmaster/flightrequestservice/FlightRequestsServiceApp.java
@@ -2,8 +2,10 @@ package ua.ikulikov.flightmaster.flightrequestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FlightRequestsServiceApp {
 
 	public static void main(String[] args) {
